c17d04f8afc5a3533263d438d249b5c3cf285ca3
ayuu02 <ayushrathor2001@gmail.com>
2025-09-24 17:33:37 +0530

    ayush demo for git

--- Server/Server.js
+++ Server/Server.js
@@ -170,4 +170,4 @@ app.listen(PORT, () => {
   console.log(`🧠 Default model: ${CHAT_MODEL}`);
   warmUp();
 });
-//here is a demo for git that how it work 
+~//here is a demo for git that how it work 
